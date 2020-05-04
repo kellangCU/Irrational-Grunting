a4ba3c266142bf52efd689631f6dd768b57170d9
Fishheadhaha <eolander@gmail.com>
2020-05-04 14:38:39 -0600

    All pages are now delivered a value if the user is logged in

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.use(cookieParser());
 
 app.use(session({
   key: 'user_sid',
-  secret: process.env.KEY,
+  //secret: process.env.KEY,
+  secret: 'remove',
   resave: false,
   saveUninitialized: false,
   cookie: {
@@ -31,20 +32,20 @@ app.use((req, res, next) => {
 
 var sessionChecker = (req, res, next) => {
   if (req.session.user && req.cookies.user_sid) {
-    res.redirect('/dashboard');
+    res.redirect('/saved-events');
   } else {
     next();
   }    
 };
 
-const dbConfig = process.env.DATABASE_URL;
-//const dbConfig = {
-//  host: 'localhost',
-//  port: 5432,
-//  database: '',
-//  user: 'postgres',
-//  password: ''
-//};
+//const dbConfig = process.env.DATABASE_URL;
+const dbConfig = {
+  host: 'localhost',
+  port: 5432,
+  database: 'igru',
+  user: 'postgres',
+  password: 'fish'
+};
 
 
 var db = pgp(dbConfig);
@@ -63,8 +64,13 @@ function stripText(input) {
 
 app.route('/signup')
   .get(sessionChecker, (req, res) => {
+    var logged_in = 0;
+    if (req.session.user && req.cookies.user_sid) {
+      logged_in = 1;
+    }
     res.render('pages/sign_up', {
-      m_title: "Sign Up"
+      m_title: "Sign Up",
+      logged_in:logged_in
     })
   })
   .post((req, res) => {
@@ -90,6 +96,7 @@ app.route('/login')
   .get(sessionChecker, (req, res) => {
     res.render('pages/login', {
       m_title:'Login',
+      logged_in:0,
       response:''
     })
   })
@@ -111,6 +118,7 @@ app.route('/login')
 app.get('/login_error', sessionChecker, (req, res) => {
   res.render('pages/login', {
     m_title: "Login",
+    logged_in:0,
     response: "Invalid username or password."
   })
 });
@@ -126,161 +134,191 @@ app.get('/logout', (req, res) => {
 
 // Landing Page
 app.get('/', function(req, res) {
+  var logged_in = 0;
+  if (req.session.user && req.cookies.user_sid) {
+    logged_in = 1;
+  }
   var query = "select * from events order by start_date_time limit 3";
   db.any(query)
     .then(function (rows) {
       res.render('pages/home', {
         m_title:"CU Boulder Event Manager",
+        logged_in:logged_in,
         local_css:"search.css",
-        data: rows
+        data: rows,
       })
     })
     .catch(function (err) {
       console.log("Error fetching events for home.");
       res.render('pages/home', {
         m_title:"CU Boulder Event Manager",
+        logged_in:logged_in,
         local_css:"search.css",
         data:""
       });
     })
 });
 
-app.get('/login', function(req, res) {
-  res.render('pages/login', {
-    m_title:"Login"
-  });
-});
-
 app.get('/saved-events', function(req, res) {
+  var logged_in = 0;
+  if (req.session.user && req.cookies.user_sid) {
+    logged_in = 1;
+  }
   res.render('pages/my_saved_events', {
-    local_css:"search.css",
-    m_title:"Saved Events"
+    m_title:"Saved Events",
+    logged_in:logged_in,
+    local_css:"search.css"
   });
 });
 
 app.get('/search', function(req, res) {
+  var logged_in = 0;
+  if (req.session.user && req.cookies.user_sid) {
+    logged_in = 1;
+  }
   res.render('pages/search', {
-    local_css:"search.css",
-    m_title:"Event Search"
+    m_title:"Event Search",
+    logged_in:logged_in,
+    local_css:"search.css"
   });
 });
 
-app.get('/registration', function(req, res) {
-  res.render('pages/registration', {
-    m_title:"Organization Registration",
-    response:'',
-    err:''
+app.route('/registration')
+  .get(function(req, res) {
+    var logged_in = 0;
+    if (req.session.user && req.cookies.user_sid) {
+      logged_in = 1;
+    }
+    res.render('pages/registration', {
+      m_title:"Organization Registration",
+      logged_in:logged_in,
+      response:'',
+      err:''
+    })
   })
-});
+  .post(function(req, res) {
+    var logged_in = 0;
+    if (req.session.user && req.cookies.user_sid) {
+      logged_in = 1;
+    }
+    var organization_name = req.body.org_name;
+    var contact = req.body.contact;
+    organization_name = stripText(organization_name);
+    contact = stripText(contact);
+    var query = "insert into organizations (org_name, contact) values('"+organization_name+"','"+contact+"')";
+    db.any(query)
+      .then(function (data) {
+        res.render('pages/registration', {
+          m_title:"Organization Registration",
+          logged_in:logged_in,
+          response:"Successfully Registered Organization",
+          err:''
+        })
+      })
+      .catch(error => {
+        console.log(error);
+        res.render('pages/registration', {
+          m_title:"Organization Registration",
+          logged_in:logged_in,
+          response:"Error Registering Organization:",
+          err: error
+        })
+      })
+  });
 
-app.post('/registration', function(req, res) {
-  var organization_name = req.body.org_name;
-  var contact = req.body.contact;
-  organization_name = stripText(organization_name);
-  contact = stripText(contact);
-  var query = "insert into organizations (org_name, contact) values('"+organization_name+"','"+contact+"')";
-  db.any(query)
-    .then(function (data) {
-      res.render('pages/registration', {
-        m_title:"Organization Registration",
-        response:"Successfully Registered Organization",
-        err:''
+app.route('/addEvent')
+  .get(function(req, res) {
+    var logged_in = 0;
+    if (req.session.user && req.cookies.user_sid) {
+      logged_in = 1;
+    }
+    var query = "select org_name,organization_id from organizations order by org_name;";
+    db.any(query)
+      .then(function (rows) {
+        res.render('pages/addEvent', {
+          m_title:'Add Event',
+          logged_in:logged_in,
+          data:rows,
+          response:''
+        })
+      })
+      .catch(function (err) {
+        console.log("Error fetching organizations for addEvent.");
+        console.log(err);
+        res.render('pages/addEvent', {
+          m_title:'Add Event',
+          logged_in:logged_in,
+          data:'',
+          response:'Error fetching organizations'
+        });
+      })
+  })
+  .post(function(req, res) {
+    var logged_in = 0;
+    if (req.session.user && req.cookies.user_sid) {
+      logged_in = 1;
+    }
+    var title = req.body.eventName;
+    var time = req.body.date;
+    var location = req.body.location;
+    var id = req.body.organization;
+    var description = req.body.description;
+    var attribute = 0;
+    if (req.body.t_academic)
+      attribute += req.body.t_academic;
+    if (req.body.t_clubs)
+      attribute += req.body.t_clubs;
+    if (req.body.t_sports)
+      attribute += req.body.t_sports;
+    if (req.body.t_fundraising)
+      attribute += req.body.t_fundraising;
+    if (req.body.t_music)
+      attribute += req.body.t_music;
+    if (req.body.t_talks)
+      attribute += req.body.t_talks;
+    if (req.body.t_other)
+      attribute += req.body.t_other;
+    var query_org = "select org_name,organization_id from organizations order by org_name;";
+    var query_insert ="insert into events "
+                    + "(event_title, start_date_time, location, attribute, description, organization_id, contact) "
+                    + "values ('"+title+"','"+time+"','"+location+"','"+attribute+"','"+description+"','"+id+"',"
+                    + "(select contact from organizations where organization_id="+id+"));";
+    //var query_org_update ="update organizations set upcoming_events=("
+    //                    + "(select upcoming_events from organizations where organization_id="req.body.organization") || ARRAY[""]"
+    db.task('get-everything', task => {
+      return task.batch([
+        task.any(query_org),
+        task.any(query_insert),
+        //task.any(query_org_update)
+      ])
+    })
+    .then(info => {
+      res.render('pages/addEvent', {
+        m_title:'Add Event',
+        logged_in:logged_in,
+        data: info[0],
+        response:'Successfully submitted event'
       })
     })
     .catch(error => {
       console.log(error);
-      res.render('pages/registration', {
-        m_title:"Organization Registration",
-        response:"Error Registering Organization:",
-        err: error
-      })
-    })
-});
-
-app.get('/addEvent', function(req, res) {
-  var query = "select org_name,organization_id from organizations order by org_name;";
-  db.any(query)
-    .then(function (rows) {
       res.render('pages/addEvent', {
         m_title:'Add Event',
-        data:rows,
-        response:''
-      })
-    })
-    .catch(function (err) {
-      console.log("Error fetching organizations for addEvent.");
-      console.log(err);
-      res.render('pages/addEvent', {
-        m_title:'Add Event',
+        logged_in:logged_in,
         data:'',
-        response:'Error fetching organizations'
-      });
+        response:'Error submitting event'
+      })
     })
-});
-
-app.post('/addEvent', function(req, res) {
-  var title = req.body.eventName;
-  var time = req.body.date;
-  var location = req.body.location;
-  var id = req.body.organization;
-  //var contact = req.body.contact;
-  var description = req.body.description;
-  var attribute = 0;
-  if (req.body.t_academic)
-    attribute += req.body.t_academic;
-  if (req.body.t_clubs)
-    attribute += req.body.t_clubs;
-  if (req.body.t_sports)
-    attribute += req.body.t_sports;
-  if (req.body.t_fundraising)
-    attribute += req.body.t_fundraising;
-  if (req.body.t_music)
-    attribute += req.body.t_music;
-  if (req.body.t_talks)
-    attribute += req.body.t_talks;
-  if (req.body.t_other)
-    attribute += req.body.t_other;
-  var query_org = "select org_name,organization_id from organizations order by org_name;";
-  var query_insert ="insert into events "
-                  + "(event_title, start_date_time, location, attribute, description, organization_id, contact) "
-                  + "values ('"+title+"','"+time+"','"+location+"','"+attribute+"','"+description+"','"+id+"',"
-                  + "(select contact from organizations where organization_id="+id+"));";
-  //var query_org_update ="update organizations set upcoming_events=("
-  //                    + "(select upcoming_events from organizations where organization_id="req.body.organization") || ARRAY[""]"
-  db.task('get-everything', task => {
-    return task.batch([
-      task.any(query_org),
-      task.any(query_insert),
-      //task.any(query_org_update)
-    ])
-  })
-  .then(info => {
-    res.render('pages/addEvent', {
-      m_title:'Add Event',
-      data: info[0],
-      response:'Successfully submitted event'
-    })
-  })
-  .catch(error => {
-    console.log(error);
-    res.render('pages/addEvent', {
-      m_title:'Add Event',
-      data:'',
-      response:'Error submitting event'
-    })
-  })
-});
-
-app.get('/signup', function(req, res) {
-  res.render('pages/signUp', {
-    local_css:"login.css",
-    local_js:"login.js",
-    m_title:"Sign Up"
   });
-});
+
 app.get('/user', function(req, res) {
+  var logged_in = 0;
+  if (req.session.user && req.cookies.user_sid) {
+    logged_in = 1;
+  }
   res.render('pages/user', {
     local_css:"calendar.css",
+    logged_in:logged_in,
     m_title:"User Calendar"
   })
 })
@@ -290,7 +328,7 @@ app.use(function (req, res, next) {
 });
 
 var port = 3000;
-port = process.env.PORT;
+//port = process.env.PORT;
 app.set('port', port);
 
 app.listen(port);
